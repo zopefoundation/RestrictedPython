661fbf6be57b53afa564feff67ab8f1d235de340
Michael Howitz <mh@gocept.com>
2016-09-22 16:06:12 +0200

    Allow a custom NodeTransformer.

--- src/RestrictedPython/compiler.py
+++ src/RestrictedPython/compiler.py
@@ -3,8 +3,13 @@ import ast
 
 
 def compile_restricted(
-        source, filename='<unknown>', mode='exec', flags=0, dont_inherit=0):
-    """Replacement for the built-in compile() function."""
+        source, filename='<unknown>', mode='exec', flags=0, dont_inherit=0,
+        policy=RestrictingNodeTransformer):
+    """Replacement for the built-in compile() function.
+
+    policy ... `ast.NodeTransformer` class defining the restrictions.
+
+    """
     c_ast = ast.parse(source, filename, mode)
-    r_ast = RestrictingNodeTransformer().visit(c_ast)
+    r_ast = policy().visit(c_ast)
     return compile(r_ast, filename, mode, flags, dont_inherit)
