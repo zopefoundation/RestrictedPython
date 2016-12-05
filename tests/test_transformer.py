4d01d7ede2be8aab625bfc0f837480e4826b7ec9
stephan-hof <sh@axiros.com>
2016-12-05 08:00:51 +0100

    Migrate 'before_and_after27'.

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -1189,3 +1189,41 @@ def test_transformer_with_stmt_subscript(compile, mocker):
 
     assert x == [0, 0, 1, 2, 0, 0, 0]
     _write_.assert_called_once_with(x)
+
+
+DICT_COMPREHENSION_WITH_ATTRS = """
+def call(seq):
+    return {y.k: y.v for y in seq.z if y.k}
+"""
+
+
+@pytest.mark.parametrize(*compile)
+def test_transformer_dict_comprehension_with_attrs(compile, mocker):
+    code, errors = compile(DICT_COMPREHENSION_WITH_ATTRS)[:2]
+
+    assert code is not None
+    assert errors == ()
+
+    _getattr_ = mocker.Mock()
+    _getattr_.side_effect = getattr
+
+    _getiter_ = mocker.Mock()
+    _getiter_.side_effect = lambda ob: ob
+
+    glb = {'_getattr_': _getattr_, '_getiter_': _getiter_}
+    six.exec_(code, glb)
+
+    z = [mocker.Mock(k=0, v='a'), mocker.Mock(k=1, v='b')]
+    seq = mocker.Mock(z=z)
+
+    ret = glb['call'](seq)
+    assert ret == {1: 'b'}
+
+    _getiter_.assert_called_once_with(z)
+    _getattr_.assert_has_calls([
+        mocker.call(seq, 'z'),
+        mocker.call(z[0], 'k'),
+        mocker.call(z[1], 'k'),
+        mocker.call(z[1], 'v'),
+        mocker.call(z[1], 'k')
+    ])
