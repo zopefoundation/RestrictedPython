c4ca8c466a4b48886c8678f7a0cc7247c0fde9b8
Michael Howitz <mh@gocept.com>
2017-01-31 19:18:50 +0100

    Clean up the tests.
    * Remove already ported tests.
    * Rename tests to match the methods they test.
    * Move tests around nearer to the code they test.
    * Add a missing visitor.

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+from RestrictedPython._compat import IS_PY2
+import RestrictedPython
+
+# Define the arguments for @pytest.mark.parametrize to be able to test both the
+# old and the new implementation to be equal:
+compile = ('compile', [RestrictedPython.compile.compile_restricted_exec])
+if IS_PY2:
+    from RestrictedPython import RCompile
+    compile[1].append(RCompile.compile_restricted_exec)
