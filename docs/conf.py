e664e1d6794fc8f97833c99c2b94da65f906cf23
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-02-09 18:13:04 +0100

    correct Copyright sentance

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'RestrictedPython'
-copyright = u'2016, Zope Foundation'
+copyright = u'2017, Zope Foundation and Contributors'
 author = u'Alexander Loechel'
 
 # The version info for the project you're documenting, acts as replacement for
