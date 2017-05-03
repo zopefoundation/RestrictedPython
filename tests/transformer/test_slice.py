fa553aae8033484aee553a4b4b146a360ae65b93
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-05-03 11:55:59 +0200

    fix spelling and convention

--- tests/transformer/test_slice.py
+++ tests/transformer/test_slice.py
@@ -10,14 +10,14 @@ def test_slice(e_eval):
     stride = 3
 
     from operator import getitem
-    rgbl = dict(_getitem_=getitem)  # restricted globals
+    rglb = {'_getitem_': getitem}  # restricted globals
 
-    assert e_eval('[1, 2, 3, 4, 5]', rgbl) == [1, 2, 3, 4, 5]
-    assert e_eval('[1, 2, 3, 4, 5][:]', rgbl) == [1, 2, 3, 4, 5]
-    assert e_eval('[1, 2, 3, 4, 5][%d:]' % low, rgbl) == [2, 3, 4, 5]
-    assert e_eval('[1, 2, 3, 4, 5][:%d]' % high, rgbl) == [1, 2, 3, 4]
-    assert e_eval('[1, 2, 3, 4, 5][%d:%d]' % (low, high), rgbl) == [2, 3, 4]
-    assert e_eval('[1, 2, 3, 4, 5][::%d]' % stride, rgbl) == [1, 4]
-    assert e_eval('[1, 2, 3, 4, 5][%d::%d]' % (low, stride), rgbl) == [2, 5]
-    assert e_eval('[1, 2, 3, 4, 5][:%d:%d]' % (high, stride), rgbl) == [1, 4]
-    assert e_eval('[1, 2, 3, 4, 5][%d:%d:%d]' % (low, high, stride), rgbl) == [2]  # NOQA: E501
+    assert e_eval('[1, 2, 3, 4, 5]', rglb) == [1, 2, 3, 4, 5]
+    assert e_eval('[1, 2, 3, 4, 5][:]', rglb) == [1, 2, 3, 4, 5]
+    assert e_eval('[1, 2, 3, 4, 5][%d:]' % low, rglb) == [2, 3, 4, 5]
+    assert e_eval('[1, 2, 3, 4, 5][:%d]' % high, rglb) == [1, 2, 3, 4]
+    assert e_eval('[1, 2, 3, 4, 5][%d:%d]' % (low, high), rglb) == [2, 3, 4]
+    assert e_eval('[1, 2, 3, 4, 5][::%d]' % stride, rglb) == [1, 4]
+    assert e_eval('[1, 2, 3, 4, 5][%d::%d]' % (low, stride), rglb) == [2, 5]
+    assert e_eval('[1, 2, 3, 4, 5][:%d:%d]' % (high, stride), rglb) == [1, 4]
+    assert e_eval('[1, 2, 3, 4, 5][%d:%d:%d]' % (low, high, stride), rglb) == [2]  # NOQA: E501
