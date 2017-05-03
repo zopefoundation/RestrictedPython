0b219c6f8ff7f5a3ed58ffe644c4814e06ce4436
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-05-03 16:37:36 +0200

    make compile_restricted_function parameterizable

--- tests/__init__.py
+++ tests/__init__.py
@@ -32,6 +32,17 @@ def _eval(compile_func):
     return _eval
 
 
+def _function(compile_func):
+    """Factory to create a function object."""
+    def _function(source, glb=None):
+        code = _compile(compile_func, source)
+        if glb is None:
+            glb = {}
+        exec(code, glb)
+        return glb
+    return _function
+
+
 # Define the arguments for @pytest.mark.parametrize to be able to test both the
 # old and the new implementation to be equal:
 # Compile in `exec` mode.
@@ -42,10 +53,15 @@ e_exec = ('e_exec', [_exec(RestrictedPython.compile.compile_restricted_exec)])
 c_eval = ('c_eval', [RestrictedPython.compile.compile_restricted_eval])
 # Compile and execute in `eval` mode.
 e_eval = ('e_eval', [_eval(RestrictedPython.compile.compile_restricted_eval)])
+#
+c_function = ('c_function', [RestrictedPython.compile.compile_restricted_function])  # NOQA: E501
+e_function = ('e_function', [_function(RestrictedPython.compile.compile_restricted_function)])  # NOQA: E501
 
 if IS_PY2:
     from RestrictedPython import RCompile
     c_exec[1].append(RCompile.compile_restricted_exec)
     c_eval[1].append(RCompile.compile_restricted_eval)
+    c_function[1].append(RCompile.compile_restricted_function)
     e_exec[1].append(_exec(RCompile.compile_restricted_exec))
     e_eval[1].append(_eval(RCompile.compile_restricted_eval))
+    e_function[1].append(_function(RCompile.compile_restricted_function))
