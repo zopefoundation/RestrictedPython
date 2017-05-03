d592aaa8a83c80e22e0f4a2c63bfb643ec1f38e4
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-05-03 10:55:58 +0200

    fix slices tests

--- tests/transformer/test_slice.py
+++ tests/transformer/test_slice.py
@@ -1,21 +1,23 @@
 from tests import e_eval
 
+import pytest
 
-def test_slice():
+
+@pytest.mark.parametrize(*e_eval)
+def test_slice(e_eval):
     low = 1
     high = 4
     stride = 3
-    
-    from operator import getitem
-    restricted_globals = dict(_getitem_=getitem)
-    restricted_eval = lambda code: e_eval[1][1](code, restricted_globals)
 
-    assert restricted_eval('[1, 2, 3, 4, 5]') == [1, 2, 3, 4, 5]
-    assert restricted_eval('[1, 2, 3, 4, 5][:]') == [1, 2, 3, 4, 5]
-    assert restricted_eval('[1, 2, 3, 4, 5][%d:]' % low) == [2, 3, 4, 5]
-    assert restricted_eval('[1, 2, 3, 4, 5][:%d]' % high) == [1, 2, 3, 4]
-    assert restricted_eval('[1, 2, 3, 4, 5][%d:%d]' % (low, high)) == [2, 3, 4]
-    assert restricted_eval('[1, 2, 3, 4, 5][::%d]' % stride) == [1, 4]
-    assert restricted_eval('[1, 2, 3, 4, 5][%d::%d]' % (low, stride)) == [2, 5]
-    assert restricted_eval('[1, 2, 3, 4, 5][:%d:%d]' % (high, stride)) == [1, 4]
-    assert restricted_eval('[1, 2, 3, 4, 5][%d:%d:%d]' % (low, high, stride)) == [2]
+    from operator import getitem
+    rgbl = dict(_getitem_=getitem)  # restricted globals
+
+    assert e_eval('[1, 2, 3, 4, 5]', rgbl) == [1, 2, 3, 4, 5]
+    assert e_eval('[1, 2, 3, 4, 5][:]', rgbl) == [1, 2, 3, 4, 5]
+    assert e_eval('[1, 2, 3, 4, 5][%d:]' % low, rgbl) == [2, 3, 4, 5]
+    assert e_eval('[1, 2, 3, 4, 5][:%d]' % high, rgbl) == [1, 2, 3, 4]
+    assert e_eval('[1, 2, 3, 4, 5][%d:%d]' % (low, high), rgbl) == [2, 3, 4]
+    assert e_eval('[1, 2, 3, 4, 5][::%d]' % stride, rgbl) == [1, 4]
+    assert e_eval('[1, 2, 3, 4, 5][%d::%d]' % (low, stride), rgbl) == [2, 5]
+    assert e_eval('[1, 2, 3, 4, 5][:%d:%d]' % (high, stride), rgbl) == [1, 4]
+    assert e_eval('[1, 2, 3, 4, 5][%d:%d:%d]' % (low, high, stride), rgbl) == [2]  # NOQA: E501
