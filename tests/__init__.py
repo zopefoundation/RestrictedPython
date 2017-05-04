100db25897508c4a307894123409c6c3c5877352
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-05-04 15:44:44 +0200

    make single parameterize

--- tests/__init__.py
+++ tests/__init__.py
@@ -34,6 +34,16 @@ def _eval(compile_func):
     return _eval
 
 
+def _single(compile_func):
+    """Factory to create an single function."""
+    def _single(source, glb=None):
+        code = _compile(compile_func, source)
+        if glb is None:
+            glb = {}
+        return exec(code, glb)
+    return _single
+
+
 def _function(compile_func):
     """Factory to create a function object."""
     def _function(source, glb=None):
@@ -59,11 +69,17 @@ e_eval = ('e_eval', [_eval(RestrictedPython.compile.compile_restricted_eval)])
 c_function = ('c_function', [RestrictedPython.compile.compile_restricted_function])  # NOQA: E501
 e_function = ('e_function', [_function(RestrictedPython.compile.compile_restricted_function)])  # NOQA: E501
 
+c_single = ('c_single', [RestrictedPython.compile.compile_restricted_single])
+e_single = ('e_single', [_single(RestrictedPython.compile.compile_restricted_single)])  # NOQA: E501
+
 if IS_PY2:
     from RestrictedPython import RCompile
     c_exec[1].append(RCompile.compile_restricted_exec)
     c_eval[1].append(RCompile.compile_restricted_eval)
+    c_single[1].append(RCompile.compile_restricted_single)
     c_function[1].append(RCompile.compile_restricted_function)
+
     e_exec[1].append(_exec(RCompile.compile_restricted_exec))
     e_eval[1].append(_eval(RCompile.compile_restricted_eval))
+    e_single[1].append(_single(RCompile.compile_restricted_single))
     e_function[1].append(_function(RCompile.compile_restricted_function))
