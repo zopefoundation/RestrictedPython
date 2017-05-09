64d0abdbaa5160476761877d8d17d71fd3fadd12
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-05-09 22:27:46 +0200

    added a test about import similar to new README

---
+++ tests/test_imports.py
@@ -0,0 +1,31 @@
+"""
+Tests about imports
+"""
+
+# Test framework imports
+import pytest
+
+# Test internals (fixures and helpers)
+from tests import c_exec
+from tests import e_exec
+
+
+OS_IMPORT_EXAMPLE = """
+import os
+
+os.listdir('/')
+"""
+
+
+@pytest.mark.parametrize(*c_exec)
+@pytest.mark.parametrize(*e_exec)
+def test_os_import(c_exec, e_exec):
+    """Test that import should not work out of the box.
+    TODO: Why does this work.
+    """
+    result = c_exec(OS_IMPORT_EXAMPLE)
+    assert result.code is not None
+    assert result.errors == ()
+
+    # with pytest.raises(NameError):
+    #    e_exec(OS_IMPORT_EXAMPLE)
