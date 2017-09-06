e3f51910ebe721f56137e2f7330625ca5b4e8ea4
Alexander Loechel <Alexander.Loechel@lmu.de>
2017-09-06 12:28:12 +0200

    fix tests

--- tests/transformer/test_import.py
+++ tests/transformer/test_import.py
@@ -21,21 +21,21 @@ def test_RestrictingNodeTransformer__visit_Import__1(c_exec):
 def test_RestrictingNodeTransformer__visit_Import__2(c_exec):
     """It denies importing a module starting with `_`."""
     result = c_exec('import _a')
-    assert result.errors == import_errmsg.format(name='_a')
+    assert result.errors == (import_errmsg.format(name='_a'),)
 
 
 @pytest.mark.parametrize(*c_exec)
 def test_RestrictingNodeTransformer__visit_Import__3(c_exec):
     """It denies importing a module starting with `_` as something."""
     result = c_exec('import _a as m')
-    assert result.errors == import_errmsg.format(name='_a')
+    assert result.errors == (import_errmsg.format(name='_a'),)
 
 
 @pytest.mark.parametrize(*c_exec)
 def test_RestrictingNodeTransformer__visit_Import__4(c_exec):
     """It denies importing a module as something starting with `_`."""
     result = c_exec('import a as _m')
-    assert result.errors == import_errmsg.format(name='_m')
+    assert result.errors == (import_errmsg.format(name='_m'),)
 
 
 @pytest.mark.parametrize(*c_exec)
@@ -58,18 +58,18 @@ def test_RestrictingNodeTransformer__visit_Import_6(c_exec):
 def test_RestrictingNodeTransformer__visit_Import__7(c_exec):
     """It denies importing from a module as something starting with `_`."""
     result = c_exec('from a import m as _n')
-    assert result.errors == import_errmsg.format(name='_n')
+    assert result.errors == (import_errmsg.format(name='_n'),)
 
 
 @pytest.mark.parametrize(*c_exec)
 def test_RestrictingNodeTransformer__visit_Import__8(c_exec):
     """It denies as-importing something starting with `_` from a module."""
     result = c_exec('from a import _m as n')
-    assert result.errors == import_errmsg.format(name='_m')
+    assert result.errors == (import_errmsg.format(name='_m'),)
 
 
 @pytest.mark.parametrize(*c_exec)
 def test_RestrictingNodeTransformer__visit_Import__9(c_exec):
     """It denies relative from importing as something starting with `_`."""
     result = c_exec('from .x import y as _leading_underscore')
-    assert result.errors == import_errmsg.format(name='_leading_underscore')
+    assert result.errors == (import_errmsg.format(name='_leading_underscore'),)
