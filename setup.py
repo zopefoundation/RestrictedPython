16dc382fd84ea5e7a1ce2c07d066eae74e398ce9
Alexander Loechel <Alexander.Loechel@lmu.de>
2018-06-30 15:44:40 +0200

    add 'Programming Language :: Python :: 3.7' to indicate that Python 3.7 is supported, even if new language Feature are not added.

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Security',
     ],
