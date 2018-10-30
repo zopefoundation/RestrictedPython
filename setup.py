90a5211b99a622931f7b7380b1919032f01d5340
Michael Howitz <mh@gocept.com>
2018-10-30 09:58:17 +0100

    Back to development: 4.0b8

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name='RestrictedPython',
-    version='4.0b7',
+    version='4.0b8.dev0',
     url='https://github.com/zopefoundation/RestrictedPython',
     license='ZPL 2.1',
     description=(
