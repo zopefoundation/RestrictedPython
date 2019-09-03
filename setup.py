75f50d391ac1b97ee0df0026c7a8035bff8ae77d
Michael Howitz <mh@gocept.com>
2019-09-03 08:16:46 +0200

    Back to development: 5.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name='RestrictedPython',
-    version='5.0',
+    version='5.1.dev0',
     url='https://github.com/zopefoundation/RestrictedPython',
     license='ZPL 2.1',
     description=(
