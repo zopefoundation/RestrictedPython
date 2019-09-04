9c94d267f64fd6c0d991510aba4713fcef1fb7df
Alexander Loechel <loechel@users.noreply.github.com>
2019-09-04 17:23:08 +0200

    Add a test for f-string self-documenting expressions. (#176)

--- tests/transformer/test_fstring.py
+++ tests/transformer/test_fstring.py
@@ -1,5 +1,7 @@
 from RestrictedPython import compile_restricted_exec
 from RestrictedPython._compat import IS_PY36_OR_GREATER
+from RestrictedPython._compat import IS_PY38_OR_GREATER
+from RestrictedPython.PrintCollector import PrintCollector
 
 import pytest
 
@@ -33,3 +35,28 @@ def test_visit_invalid_variable_name():
     assert result.errors == (
         'Line 1: "__init__" is an invalid variable name because it starts with "_"',  # NOQA: E501
     )
+
+
+f_string_self_documenting_expressions_example = """
+from datetime import date
+
+user = 'eric_idle'
+member_since = date(1975, 7, 31)
+print(f'{user=} {member_since=}')
+"""
+
+
+@pytest.mark.skipif(
+    not IS_PY38_OR_GREATER,
+    reason="f-string self-documenting expressions added in Python 3.8.",
+)
+def test_f_string_self_documenting_expressions():
+    """Checks if f-string self-documenting expressions is checked."""
+    result = compile_restricted_exec(
+        f_string_self_documenting_expressions_example,
+    )
+    assert result.errors == ()
+
+    glb = {'_print_': PrintCollector, '_getattr_': None}
+    exec(result.code, glb)
+    assert glb['_print']() == "user='eric_idle' member_since=datetime.date(1975, 7, 31)\n"  # NOQA: E501
