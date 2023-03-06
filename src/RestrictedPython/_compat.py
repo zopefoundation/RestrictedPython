0367ea69f7fbff75575ca6fd1bf9e5199c8c66a1
Michael Howitz <mh@gocept.com>
2023-03-06 09:25:21 +0100

    Drop support for Python 3.6.

--- src/RestrictedPython/_compat.py
+++ src/RestrictedPython/_compat.py
@@ -3,7 +3,6 @@ import sys
 
 
 _version = sys.version_info
-IS_PY37_OR_GREATER = _version.major == 3 and _version.minor >= 7
 IS_PY38_OR_GREATER = _version.major == 3 and _version.minor >= 8
 IS_PY310_OR_GREATER = _version.major == 3 and _version.minor >= 10
 IS_PY311_OR_GREATER = _version.major == 3 and _version.minor >= 11
