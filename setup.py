f25c119898ec944d4fd3169a6086a49ea3236eb5
Jens Vagelpohl <jens@plyp.com>
2023-07-08 10:12:47 +0200

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name='RestrictedPython',
-    version='5.3',
+    version='5.4.dev0',
     url='https://github.com/zopefoundation/RestrictedPython',
     license='ZPL 2.1',
     description=(
