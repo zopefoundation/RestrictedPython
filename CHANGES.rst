f9ef13be3f38cb55fb6f714c608075e3c895032e
Jens Vagelpohl <jens@plyp.com>
2023-07-08 10:10:16 +0200

    - prepare release 5.3

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changes
 =======
 
-5.3 (unreleased)
+5.3 (2023-07-08)
 ----------------
 
 - Restrict access to some attributes accessible via the ``inspect`` module.
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name='RestrictedPython',
-    version='5.3.dev0',
+    version='5.3',
     url='https://github.com/zopefoundation/RestrictedPython',
     license='ZPL 2.1',
     description=(
