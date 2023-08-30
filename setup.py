7031e2d8d83ec5cdbe2b38c78beab0cdab565ee8
Michael Howitz <icemac@gmx.net>
2023-08-30 08:29:57 +0200

    Preparing release 5.4

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name='RestrictedPython',
-    version='5.4.dev0',
+    version='5.4',
     url='https://github.com/zopefoundation/RestrictedPython',
     license='ZPL 2.1',
     description=(
