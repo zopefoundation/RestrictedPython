c265a8f4b3acac90be9de7d0b40c9827f014e8fe
Michael Howitz <icemac@gmx.net>
2023-11-17 08:19:22 +0100

    Back to development: 7.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ tests_require = [
 
 setup(
     name='RestrictedPython',
-    version='7.0',
+    version='7.1.dev0',
     url='https://github.com/zopefoundation/RestrictedPython',
     license='ZPL 2.1',
     description=(
